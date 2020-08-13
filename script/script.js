044ce838cffaee5c45be5d2dc3ec4e25798d5e6b
Arthur Segato <arthursegato@ctemplar.com>
2020-08-13 19:43:08 -0300

    changed files

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+/* OPEN MENU ON CLICK AND BLOCK SCROLL */
 let open = false;
 function toggleMenu() {
 	let btn = document.getElementById('menu-btn');
@@ -5,11 +6,22 @@ function toggleMenu() {
 	if (open == false) {
 		document.getElementById("sideNav").style.width = "100%";
 		open = true;
+        let x = window.scrollX, y = window.scrollY;
+        window.onscroll = function(){
+            window.scrollTo(x, y);
+        };
 	}else{
 		document.getElementById("sideNav").style.width = "0%";
 		open = false;
+        window.onscroll = function(){};
 	}
 }
+/* SHOW SOCIAL ICONS*/
+function show(){
+    let social = document.getElementById('social-icons');
+    social.classList.toggle("change");
+}
+/* SET SCROLL TO SMOOTH AND CLEAN URL*/
 let links = document.getElementsByTagName("a");
 Array.prototype.forEach.call(links, function(elem, index) {
     let elemAttr = elem.getAttribute("href");
